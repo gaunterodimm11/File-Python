5ef0061452289dfd345382a41844ff5ae8c86e71
Gia N <gianusantara@gmail.com>
2023-02-15 22:39:47 +0700

    Pascal

---
+++ Input Output Pascal.pas
@@ -0,0 +1,16 @@
+program InputOutputExample;
+var
+   inputString: string;
+   inputNumber: Integer;
+
+begin
+   Write('Masukkan sebuah string: ');
+   Readln(inputString);
+   Writeln('String yang dimasukkan adalah: ', inputString);
+
+   Write('Masukkan sebuah angka: ');
+   Readln(inputNumber);
+   Writeln('Angka yang dimasukkan adalah: ', inputNumber);
+
+   Readln;
+end.
