e027db4f40073499f4c3e1abd2c45ba3fb5cf226
Gia N <gianusantara@gmail.com>
2023-02-16 09:44:50 +0700

    TP Modul 2

---
+++ TP_MOD2.pas
@@ -0,0 +1,19 @@
+program cari_terbesar;
+var
+  a, b, c, terbesar: integer;
+begin
+  write('Masukkan bilangan pertama: ');
+  readln(a);
+  write('Masukkan bilangan kedua  : ');
+  readln(b);
+  write('Masukkan bilangan ketiga: ');
+  readln(c);
+
+  terbesar := a;
+  if b > terbesar then
+    terbesar := b;
+  if c > terbesar then
+    terbesar := c;
+
+  writeln('Bilangan terbesar adalah: ', terbesar);
+end.
